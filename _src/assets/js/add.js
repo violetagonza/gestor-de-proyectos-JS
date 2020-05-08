f709ca6990e3ce51b9b72ad0aaa8835892e5f66f
violetagonza <violetagonzalezcalleja@gmail.com>
2020-05-08 09:49:32 +0200

    ::bug: Fix unique ID to new cards with Date.now

--- _src/assets/js/add.js
+++ _src/assets/js/add.js
@@ -2,12 +2,12 @@
 let num = 0;
 const handleAddCard = (ev) => {
   clickedID = ev.currentTarget.id;
-  num += 10;
+  //   num += 10;
   for (let i = 0; i < data.length; i++) {
     console.log(num);
     if (data[i].id === parseInt(clickedID)) {
       data[i].cards.push({
-        id: num.toString(),
+        id: Date.now().toString(),
         title: 'Título de la tarea',
         description: 'descripción de la tarea',
         tags: ['JS', 'Css', 'Html'],
